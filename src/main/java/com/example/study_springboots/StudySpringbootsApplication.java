60bd0287075f935d07d7c8ce0b6e776541c0f762
ppquokka <quokka2335@gmail.com>
2023-06-16 18:21:39 +0900

    first commit

---
+++ src/main/java/com/example/study_springboots/StudySpringbootsApplication.java
@@ -0,0 +1,15 @@
+package com.example.study_springboots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan //서블릿 자동 등록
+@SpringBootApplication
+public class StudySpringbootsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySpringbootsApplication.class, args);
+	}
+
+}
